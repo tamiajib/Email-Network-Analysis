d1eff60d833972136b994fcbd5f506baa1860f27
tamiajib <144944949+tamiajib@users.noreply.github.com>
2024-04-24 18:24:45 -0400

    Read File (Extract Sender & Recipient IDs)

--- ena_ds210/ena_code/src/main.rs
+++ ena_ds210/ena_code/src/main.rs
@@ -1,3 +1,20 @@
+use std::collections::HashMap;
+use std::fs:File;
+use std::io::BufReader;
+
 fn main() {
-    println!("Testing Source Code");
+    let file = File::open("email-Enron.txt").expect("Failed to open file");
+    let reader = BufReader::new(file);
+    let mut edges: Vec<(usize, usize)> = vec![];
+
+    for line in reader.lines().skip(5) {
+        let line = line.expect("Failed to read line");
+        let parts: Vec<&str> = line.split('\t').collect();
+        if parts.len() == 2 {
+            let s_node: usize = parts[0].parse().unwrap(); 
+            let r_node: usize = parts[1].parse().unwrap(); 
+            edges.push((s_node, r_node));
+
+        }
+    }
 }
